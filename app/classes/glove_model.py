ac497509fa39a939f61c6436f814728dbe5d3684
Swaraj Bhanja <st125052@ait.ac.th>
2025-01-19 01:32:29 +0700

    Directory Restructuring and Initial Testing

---
+++ app/classes/glove_model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+class Glove(nn.Module):
+    
+    def __init__(self, voc_size, emb_size, vocab=None):
+        super(Glove, self).__init__()
+        self.center_embedding  = nn.Embedding(voc_size, emb_size)
+        self.outside_embedding = nn.Embedding(voc_size, emb_size)
+        
+        self.center_bias       = nn.Embedding(voc_size, 1) 
+        self.outside_bias      = nn.Embedding(voc_size, 1)
+
+        self.vocab = vocab if vocab is not None else {}
+    
+    def forward(self, center, outside, coocs, weighting):
+        center_embeds  = self.center_embedding(center) #(batch_size, 1, emb_size)
+        outside_embeds = self.outside_embedding(outside) #(batch_size, 1, emb_size)
+        
+        center_bias    = self.center_bias(center).squeeze(1)
+        target_bias    = self.outside_bias(outside).squeeze(1)
+        
+        inner_product  = outside_embeds.bmm(center_embeds.transpose(1, 2)).squeeze(2)
+        #(batch_size, 1, emb_size) @ (batch_size, emb_size, 1) = (batch_size, 1, 1) = (batch_size, 1)
+        
+        loss = weighting * torch.pow(inner_product + center_bias + target_bias - coocs, 2)
+        
+        return torch.sum(loss)
+    
+    def __contains__(self, word):
+        # Check if the word is in the vocabulary
+        return word in self.vocab
